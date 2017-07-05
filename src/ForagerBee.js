d2500b8bbc90c6187b1a3975d65fe1f0675501a6
Jesus Esquivel <JAER@Jesuss-MacBook-Air.local>
2017-07-05 08:09:33 -0500

    Completed All of Specs

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,22 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
+
+
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype = ForagerBee;
+
+
+ForagerBee.prototype.forage = function(ele){
+  this.treasureChest.push(ele);
+}
+
+
+
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,10 @@
 var Grub = function() {
+  this.age = 10,
+  this.color = 'pink',
+  this.food = 'jelly'
+
 };
 
+Grub.prototype.eat = function() {
+
+}
